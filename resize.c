a4ce9dcb6755c6f068a88e2963f734354a1f5e3a
Parth Sameer Kalgaonkar <f2016268@pilani.bits-pilani.ac.in>
2017-03-22 06:02:10 +0000

    scale>1.0 handled

--- resize.c
+++ resize.c
@@ -7,11 +7,9 @@
 
 #include "bmp.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     // ensure proper usage
-    if (argc != 4)
-    {
+    if (argc != 4){
         fprintf(stderr, "Usage: ./copy scale infile outfile\n");
         return 1;
     }
@@ -25,16 +23,14 @@ int main(int argc, char *argv[])
 
     // open input file 
     FILE *inptr = fopen(infile, "r");
-    if (inptr == NULL)
-    {
+    if (inptr == NULL){
         fprintf(stderr, "Could not open %s.\n", infile);
         return 2;
     }
 
     // open output file
     FILE *outptr = fopen(outfile, "w");
-    if (outptr == NULL)
-    {
+    if (outptr == NULL){
         fclose(inptr);
         fprintf(stderr, "Could not create %s.\n", outfile);
         return 3;
@@ -50,8 +46,7 @@ int main(int argc, char *argv[])
 
     // ensure infile is (likely) a 24-bit uncompressed BMP 4.0
     if (in_bf.bfType != 0x4d42 || in_bf.bfOffBits != 54 || in_bi.biSize != 40 || 
-        in_bi.biBitCount != 24 || in_bi.biCompression != 0)
-    {
+        in_bi.biBitCount != 24 || in_bi.biCompression != 0){
         fclose(outptr);
         fclose(inptr);
         fprintf(stderr, "Unsupported file format.\n");
@@ -80,33 +75,70 @@ int main(int argc, char *argv[])
 
     // determine padding for scanlines
     int padding_old = (4 - (in_bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+    
+    //counters
+    int i,j,k,l=4;
+    
+    if(scale==1.0){
+        // iterate over infile's scanlines
+        for (i = 0, biHeight = abs(in_bi.biHeight); i < biHeight; i++){
+            // iterate over pixels in scanline
+            for (j = 0; j < in_bi.biWidth; j++){
+                // temporary storage
+                RGBTRIPLE triple;
+    
+                // read RGB triple from infile
+                fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+    
+                // write RGB triple to outfile
+                fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+            }
 
-    // iterate over infile's scanlines
-    for (int i = 0, biHeight = abs(in_bi.biHeight); i < biHeight; i++)
-    {
-        // iterate over pixels in scanline
-        for (int j = 0; j < in_bi.biWidth; j++)
-        {
-            // temporary storage
-            RGBTRIPLE triple;
+            // skip over padding, if any
+            fseek(inptr, padding_old, SEEK_CUR);
 
-            // read RGB triple from infile
-            fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
-
-            // write RGB triple to outfile
-            fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
-        }
-
-        // skip over padding, if any
-        fseek(inptr, padding_old, SEEK_CUR);
-
-        // then add it back (to demonstrate how)
-        for (int k = 0; k < padding_new; k++)
-        {
-            fputc(0x00, outptr);
+            // then add it back (to demonstrate how)
+            for (k = 0; k < padding_new; k++){
+                fputc(0x00, outptr);
+            }
         }
     }
-
+    else{
+        if(scale>1.0){
+            // iterate over infile's scanlines
+            for (i = 0, biHeight = abs(in_bi.biHeight); i < biHeight; i++){
+                // iterate over pixels in scanline
+                for (j = 0; j < in_bi.biWidth; j++){
+                    // temporary storage
+                    RGBTRIPLE triple;
+    
+                    // read RGB triple from infile
+                    fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
+        
+                    // write RGB triple to outfile
+                    fwrite(&triple, sizeof(RGBTRIPLE), scale, outptr);
+                }
+    
+                // skip over padding, if any
+                fseek(inptr, padding_old, SEEK_CUR);
+    
+                // then add it back (to demonstrate how)
+                for (int k = 0; k < padding_new; k++){
+                    fputc(0x00, outptr);
+                }
+                
+                l--;
+                if(l>0){
+                    fseek(inptr, -(padding_old+in_bi.biwidth), SEEK_CUR);
+                    i--;
+                }
+                if(l==0) l=4;
+            }
+        }
+        else{
+            float mult=1/scale;
+        }
+    }
     // close infile
     fclose(inptr);
 
